b2ebda81cc53641214f148190556901a149a1a54
Vithusha <mohanvithushana94@gmail.com>
2020-10-24 19:12:37 +0530

    migration for studentand class

---
+++ database/migrations/2020_10_24_085535_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('class_id')->unsigned()->index();
+            $table->foreign('class_id')->references('id')->on('classrooms')->onDelete('cascade');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('gender');
+            $table->string('joined_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
